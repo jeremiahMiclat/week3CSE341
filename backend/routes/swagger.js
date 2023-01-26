7028d2736790f469414a6ecc88d26954eecb289a
jeremiahMiclat <mic19017@byui.edu>
2023-01-27 04:11:50 +0800

    Updated route /

--- backend/routes/swagger.js
+++ backend/routes/swagger.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument = require('../swagger_output.json');
+const controller = require('../controllers/contacts')
 
+// get all contacts
+router.get('/', controller.getData)
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
