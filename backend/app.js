0a5331a44660f70860174a93eaeb2917c77a7c54
jeremiahMiclat <mic19017@byui.edu>
2023-01-27 03:55:13 +0800

    Access-Control-Allow-Headers

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app
         res.setHeader('Acess-Control-Allow-Origin', '*');
         res.setHeader(
             'Access-Control-Allow-Headers',
-            'Origin, X-Requested-Width, Content-Type, Accept, Z-Key'
+            '*'
         )
         res.setHeader('Content-Type', 'application/json')
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
